42212f0977452eba3df5281a730c6185968c88ca
21206633 <21206633@ppti-14-305-01.ufr-info-p6.jussieu.fr>
2025-10-07 17:49:15 +0200

    tme 2 rendu final

--- Java/src/com/paracamplus/ilp1/ilp1tme2/ex2/CountConstants.java
+++ Java/src/com/paracamplus/ilp1/ilp1tme2/ex2/CountConstants.java
@@ -4,25 +4,36 @@ import com.paracamplus.ilp1.interfaces.IASTalternative;
 import com.paracamplus.ilp1.interfaces.IASTbinaryOperation;
 import com.paracamplus.ilp1.interfaces.IASTblock;
 import com.paracamplus.ilp1.interfaces.IASTboolean;
+import com.paracamplus.ilp1.interfaces.IASTexpression;
 import com.paracamplus.ilp1.interfaces.IASTfloat;
 import com.paracamplus.ilp1.interfaces.IASTinteger;
 import com.paracamplus.ilp1.interfaces.IASTinvocation;
-import com.paracamplus.ilp1.interfaces.IASToperator;
+import com.paracamplus.ilp1.interfaces.IASTprogram;
 import com.paracamplus.ilp1.interfaces.IASTsequence;
 import com.paracamplus.ilp1.interfaces.IASTstring;
 import com.paracamplus.ilp1.interfaces.IASTunaryOperation;
 import com.paracamplus.ilp1.interfaces.IASTvariable;
 import com.paracamplus.ilp1.interfaces.IASTvisitor;
+import com.paracamplus.ilp1.interfaces.IASTblock.IASTbinding;
 import com.paracamplus.ilp1.interpreter.interfaces.EvaluationException;
-import com.paracamplus.ilp1.interpreter.interfaces.ILexicalEnvironment;
-import com.paracamplus.ilp1.interpreter.interfaces.IOperator;
+import com.paracamplus.ilp1.interpreter.interfaces.Invocable;
 
 public class CountConstants implements IASTvisitor<Integer, Void, EvaluationException> {
-
+	
+	public int visit(IASTprogram iast, Void data) throws EvaluationException {
+        try {
+            return iast.getBody().accept(this, null);
+        } catch (Exception exc) {
+        	exc.printStackTrace();
+            return 0;
+        }
+    }
+	
 	@Override
 	public Integer visit(IASTalternative iast, Void data) throws EvaluationException {
-		// TODO Auto-generated method stub
-		return null;
+        return iast.getCondition().accept(this, null) 
+        		+ iast.getConsequence().accept(this, null) 
+        		+ iast.getAlternant().accept(this, null);
 	}
 
 	@Override
@@ -34,8 +45,11 @@ public class CountConstants implements IASTvisitor<Integer, Void, EvaluationExce
 
 	@Override
 	public Integer visit(IASTblock iast, Void data) throws EvaluationException {
-		// TODO Auto-generated method stub
-		return null;
+		int res = 0;
+        for ( IASTbinding binding : iast.getBindings() ) {
+            res += binding.getInitialisation().accept(this, null);
+        }
+        return res;
 	}
 
 	@Override
@@ -55,14 +69,26 @@ public class CountConstants implements IASTvisitor<Integer, Void, EvaluationExce
 
 	@Override
 	public Integer visit(IASTinvocation iast, Void data) throws EvaluationException {
-		// TODO Auto-generated method stub
-		return null;
+		Object function = iast.getFunction().accept(this, null);
+        if ( function instanceof Invocable ) {
+            int res = 0;
+            for ( IASTexpression arg : iast.getArguments() ) {
+                res += arg.accept(this, null);
+            }
+            return res;
+        } else {
+            return 0;
+        }
 	}
 
 	@Override
 	public Integer visit(IASTsequence iast, Void data) throws EvaluationException {
-		// TODO Auto-generated method stub
-		return null;
+		IASTexpression[] expressions = iast.getExpressions();
+        int res = 0;
+        for ( IASTexpression e : expressions ) {
+            res += e.accept(this, null);
+        }
+        return res;
 	}
 
 	@Override
@@ -78,7 +104,7 @@ public class CountConstants implements IASTvisitor<Integer, Void, EvaluationExce
 
 	@Override
 	public Integer visit(IASTvariable iast, Void data) throws EvaluationException {
-		return 1;
+		return 0;
 	}
 	
 	
---
+++ Java/src/com/paracamplus/ilp1/ilp1tme2/ex2/ILPMLParserEx2p2.java
@@ -0,0 +1,48 @@
+package com.paracamplus.ilp1.ilp1tme2.ex2;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import antlr4.ILPMLgrammar1Lexer;
+import antlr4.ILPMLgrammar1Parser;
+import com.paracamplus.ilp1.interfaces.IASTfactory;
+import com.paracamplus.ilp1.interfaces.IASTprogram;
+import com.paracamplus.ilp1.parser.ParseException;
+import com.paracamplus.ilp1.parser.ilpml.ILPMLParser;
+
+public class ILPMLParserEx2p2 extends ILPMLParser {
+
+	public ILPMLParserEx2p2(IASTfactory factory) {
+		super(factory);
+	}
+
+	@Override
+    public IASTprogram getProgram() throws ParseException {
+		try {
+			ANTLRInputStream in = new ANTLRInputStream(input.getText());
+			// flux de caractères -> analyseur lexical
+			ILPMLgrammar1Lexer lexer = new ILPMLgrammar1Lexer(in);
+			// analyseur lexical -> flux de tokens
+			CommonTokenStream tokens =	new CommonTokenStream(lexer);
+			// flux tokens -> analyseur syntaxique
+			ILPMLgrammar1Parser parser =	new ILPMLgrammar1Parser(tokens);
+			// démarage de l'analyse syntaxique
+			ILPMLgrammar1Parser.ProgContext tree = parser.prog();		
+			// parcours de l'arbre syntaxique et appels du Listener
+			ParseTreeWalker walker = new ParseTreeWalker();
+			ILPMLListenerEx2 extractor = new ILPMLListenerEx2(factory);
+			walker.walk(extractor, tree);
+			
+			IASTprogram ast = tree.node;
+	        CountConstants countConstants = new CountConstants();
+	        int count = countConstants.visit(ast, null);
+	        System.out.println("Nombre de constantes dans l'AST : " + count);
+
+			return ast;
+		} catch (Exception e) {
+			throw new ParseException(e);
+		}
+    }
+
+}
